317e737dadca6e19e9bbd1b4134ae430259a1773
Hugo Leon <hugoleon@outlook.com>
2023-11-06 13:24:34 -0300

    45% testes requisito 29

--- src/tests/RecipeDetails.test.tsx
+++ src/tests/RecipeDetails.test.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import RecipeDetails from '../page/RecipeDetails';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
+// import { BrowserRouter } from 'react-router-dom';
+// import RecipeDetails from '../page/RecipeDetails';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
 
+const rota = '/meals/53013';
 test('renders loading message if recipeDetails is not available', () => {
-  render(
-    <BrowserRouter>
-      <RecipeDetails />
-    </BrowserRouter>,
-  );
+  renderWithRouter(<App />, { route: rota });
 
   const loadingMessage = screen.getByText(/Loading.../i);
   expect(loadingMessage).toBeInTheDocument();
 });
 
 test('Renderiza o componente RecipeDetails', () => {
-  render(
-    <BrowserRouter>
-      <RecipeDetails />
-    </BrowserRouter>,
-  );
+  renderWithRouter(<App />, { route: rota });
+});
+
+test('Testa fetch detalhes', async () => {
+  // const fetchResolvedValue = {
+  //   json: async () => mockRecipeResponse,
+  // } as Response;
+
+  // vi.spyOn(global, 'fetch')
+  //   .mockResolvedValue(fetchResolvedValue);
+
+  renderWithRouter(<App />, { route: rota });
+
+  await waitForElementToBeRemoved(() => screen.getByText(/Loading.../i));
+  const nameRecipe = screen.getByText('Big Mac');
+  expect(nameRecipe).toBeInTheDocument();
 });
